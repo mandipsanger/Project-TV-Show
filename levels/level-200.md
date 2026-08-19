f22541fab451f15b49262ab6d8f244b8c7539993
mandip sanger <mandipsanger1@gmail.com>
2026-08-19 23:45:34 +0100

    level 200

--- levels/level-200.md
+++ levels/level-200.md
@@ -17,6 +17,7 @@ Now, work individually to complete level 200:
 Look at their level 100 implementation.
 
 Compare their implementation to yours. Think:
+
 1. How is it different?
 2. What do you prefer about your implementation?
 3. What do you prefer about their implementation?
@@ -31,6 +32,7 @@ Have a discussion about your answers to these questions. In class, together you 
 **Before implementing new features**, take some time to change the codebase you're going to build level 200 in. Change anything you think will make it easier to add more features.
 
 Some example ideas that you may want to think about:
+
 1. Could any variables or functions have more clear names, to help you understand what they do?
 2. Would [extracting functions](https://code.visualstudio.com/docs/editor/refactoring) help make some code easier to understand?
 
@@ -42,7 +44,6 @@ Have them review, and when happy, merge your PR.
 
 ![Select the partner repo in the dropdown when making a PR for level200](pr-level200.png)
 
-
 ## Adding new functionality
 
 Level 200 is all about being able to filter episodes.
@@ -51,12 +52,13 @@ Level 200 is all about being able to filter episodes.
 
 Add a live search input which meets the following requirements:
 
-When a user types a search term into the search box:
+s shoulWhen a user types a search term into the search box:
+
 1. Only episodes whose summary **OR** name contains the search term should be displayed
 2. The search should be case-**in**sensitive
 3. The display should update **immediately** after each keystroke changes the input
 4. Display how many episodes match the current search
-5. If the search box is cleared, **all** episodes should be shown
+5. If the search box is cleared, **all** episoded be shown
 
 Send a pull request to your partner's repo with this functionality. Have them review, and when happy, merge your PR.
 
@@ -71,6 +73,7 @@ Here is one example layout.
 ### Episode selector
 
 Add a `select` drop-down which lets the user jump quickly to a particular episode, with the following requirements:
+
 1. The select input should list all episodes in the format: "S01E01 - Winter is Coming"
 2. When the user makes a selection, they should be taken directly to that episode in the list
 3. Bonus: if you prefer, when the select is used, ONLY show the selected episode. If you do this, be sure to provide a way for the user to see all episodes again.
--- script.js
+++ script.js
@@ -3,45 +3,61 @@ function setup() {
   const allEpisodes = getAllEpisodes();
 
   makePageForEpisodes(allEpisodes);
-}
-function makeEpisodeCode(season, episodeNumber){
-  season = String(season).padStart(2,"0")
-  episodeNumber = String(episodeNumber).padStart(2,"0")
-  const episodeCode = "S" + season +"E" + episodeNumber
-  return episodeCode
+  setupSearch(allEpisodes); //update setup()
 }
 
+function setupSearch(allEpisodes) {
+  const searchInput = document.getElementById("search");
+
+  searchInput.addEventListener("input", function () {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      return (
+        episode.name.toLowerCase().includes(searchTerm) ||
+        episode.summary.toLowerCase().includes(searchTerm)
+      );
+    });
+
+    makePageForEpisodes(filteredEpisodes);
+  });
+}
+
+function makeEpisodeCode(season, episodeNumber) {
+  season = String(season).padStart(2, "0");
+  episodeNumber = String(episodeNumber).padStart(2, "0");
+  const episodeCode = "S" + season + "E" + episodeNumber;
+  return episodeCode;
+}
 
 function makePageForEpisodes(episodeList) {
-  
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
-
-  // Goal: a function to create the page for the all episodes 
-  // get the data from the array episode 
+  // Goal: a function to create the page for the all episodes
+  // get the data from the array episode
   // show number of episodes
-  // loop through each episode and display information 
-  
-  for(const episode of episodeList){
-    const episodeCards = document.getElementById("episode_cards_template").content.cloneNode(true);
-    const code = makeEpisodeCode(episode.season, episode.number)
-    episodeCards.querySelector("#name").textContent = episode.name
-    // Make a use of the makeEpisodeCode for the episode number content 
-    episodeCards.querySelector("#EpisodeNumber").textContent = code
+  // loop through each episode and display information
 
-    // get access to the image to assign the alt and src attributes 
-    const image = episodeCards.querySelector("#image")
-    image.src = episode.image.medium
-    image.alt =`Scene from ${code},${episode.name}`
+  for (const episode of episodeList) {
+    const episodeCards = document
+      .getElementById("episode_cards_template")
+      .content.cloneNode(true);
+    const code = makeEpisodeCode(episode.season, episode.number);
+    episodeCards.querySelector("#name").textContent = episode.name;
+
+    // Make a use of the makeEpisodeCode for the episode number content
+    episodeCards.querySelector("#EpisodeNumber").textContent = code;
+
+    // get access to the image to assign the alt and src attributes
+    const image = episodeCards.querySelector("#image");
+    image.src = episode.image.medium;
+    image.alt = `Scene from ${code},${episode.name}`;
+
     // get the summary of the episode
-    episodeCards.querySelector("#summary").textContent = episode.summary
+    episodeCards.querySelector("#summary").textContent = episode.summary;
     document.getElementById("root").append(episodeCards);
-
   }
-  
 }
 
 window.onload = setup;
-
-
